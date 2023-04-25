3018cd7a68e68cc65ef988e53bccab9260ddd1c6
Kaden Johnson <howell.johnson@outlook.com>
2023-04-25 09:50:10 -0500

    Logic to grab two stones and get the difference

--- last_stone_weight.cpp
+++ last_stone_weight.cpp
@@ -1,1 +1,25 @@
 //Last Stone Weight
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    //Test info
+    vector<int> stones{2,7,4,1,8,1};
+    //Variables
+    vector<int>::const_iterator stone_one;
+    vector<int>::const_iterator stone_two;
+    int difference;
+    //Order vector
+    sort(stones.begin(), stones.end());
+    //Grab two heaviest stones
+    stone_one = stones.end() - 1;
+    stone_two = stones.end() - 2;
+    //Subtract
+    difference = *stone_one - *stone_two;
+
+    cout << difference << endl;
+    return 0;
+}

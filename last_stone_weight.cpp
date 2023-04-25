64f37e8c4b4c9f317ff5fafb67c705010adefc2b
Kaden Johnson <howell.johnson@outlook.com>
2023-04-25 10:49:08 -0500

    solved the problem

--- last_stone_weight.cpp
+++ last_stone_weight.cpp
@@ -7,11 +7,13 @@ using namespace std;
 
 int main(){
     //Test info
-    vector<int> stones{2,7,4,1,8,1};
+    vector<int> stones{2,6,4,1,8,1};
     //Variables
     vector<int>::const_iterator stone_one;
     vector<int>::const_iterator stone_two;
     int difference;
+    //Loop over information
+    while(stones.size() > 1){
     //Order vector
     sort(stones.begin(), stones.end());
     //Grab two heaviest stones
@@ -19,7 +21,13 @@ int main(){
     stone_two = stones.end() - 2;
     //Subtract
     difference = *stone_one - *stone_two;
-
-    cout << difference << endl;
+    //Remove the two heaviest stones
+    stones.pop_back();
+    stones.pop_back();
+    //Add the new stone
+    stones.push_back(difference);
+    }
+    stone_one = stones.begin();
+    cout << *stone_one << endl; 
     return 0;
 }
